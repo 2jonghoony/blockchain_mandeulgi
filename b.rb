7598c95931203928f63371cfef8adddfcbc761ef
2jonghoony <dlwhdgns0402@gmail.com>
2018-04-24 21:55:19 +0900

    new file
    making b.rb

---
+++ b.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+
+class Dog
+	def eat
+		puts "Eating!!"
+	end
+
+	def run
+		puts "Running!!"
+	end
+
+	def walk
+		puts "Walking!!"
+	end
+end
+
+new_dog = Dog.new
+new_dog.run
