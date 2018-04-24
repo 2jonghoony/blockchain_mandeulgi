8b583ab2f81d0888aa117c9e5e676d17b58a33b8
2jonghoony <dlwhdgns0402@gmail.com>
2018-04-24 22:51:48 +0900

    Test
    for Dog class on Server

--- b.rb
+++ b.rb
@@ -4,31 +4,11 @@ class Dog
 
 	def initialize
 	 @weight = 0.5
-	 @prev_weight = 0.5
-	 @age = 1
 	end
 	def my_weight
-		puts "Prev weight : " + @prev_weight.to_s
-		puts "Now weight : " + @weight.to_s
+		@weight.to_s
 	end
 	def eat
-		@prev_weight = @weight
 		@weight += 0.5
-		puts "Eating!!"
-	end
-
-	def run
-		puts "Running!!"
-	end
-
-	def walk
-		puts "Walking!!"
 	end
 end
-
-baekgoo = Dog.new
-heukgoo = Dog.new
-baekgoo.my_weight
-baekgoo.eat
-baekgoo.my_weight
-heukgoo.my_weight
--- server.rb
+++ server.rb
@@ -1,11 +1,25 @@
 # encoding: UTF-8
 
 require 'sinatra'
+require './b'
 
+baekgoo = Dog.new
+
+=begin
 get '/mining' do 
 	"<h1 style='text-align:center'>회원가입 페이지입니다.</h1>"
 end
 
 get '/make_a_transaction' do
 	'로그아웃하셨습니다.'
+end 
+=end
+
+get '/' do
+	"Dog " + baekgoo.my_weight
+end
+
+get '/eat' do
+	baekgoo.eat
+	'먹었음'
 end
