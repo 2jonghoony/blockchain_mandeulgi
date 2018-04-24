a69046349952e02b39fa37e3a2fe2f2aec509711
2jonghoony <dlwhdgns0402@gmail.com>
2018-04-24 21:32:42 +0900

    mining and make_a_transaction
    mining and make_a_transaction

--- server.rb
+++ server.rb
@@ -2,6 +2,10 @@
 
 require 'sinatra'
 
-get '/join' do 
-	"회원가입 페이지입니다."
+get '/mining' do 
+	"<h1 style='text-align:center'>회원가입 페이지입니다.</h1>"
+end
+
+get '/make_a_transaction' do
+	'로그아웃하셨습니다.'
 end
